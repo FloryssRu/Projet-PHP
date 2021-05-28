ac755915bff0bc4f1449a83cabfce42152c54128
Floryss Rubechi <floryss.rubechi@gmail.com>
2021-05-28 10:39:12 +0200

    solve 2 code smells to merge branch into main

--- src/Services/AdminProtectionPart.php
+++ src/Services/AdminProtectionPart.php
@@ -7,6 +7,11 @@ use App\Core\BaseController;
 class AdminProtectionPart extends BaseController
 {
 
+    /**
+     * Is empty because the mother class has a construct function and we don't want to use it for this class.
+     *
+     * @return void
+     */
     public function __construct()
     {
 
@@ -15,7 +20,7 @@ class AdminProtectionPart extends BaseController
     public function redirectNotAdmin()
 	{
         $session = new PHPSession;
-		if($session->get('admin') == NULL || $session->get('admin') == false)
+		if($session->get('admin') == NULL || !$session->get('admin'))
         {
             $this->redirect('/erreur-403');
         }
