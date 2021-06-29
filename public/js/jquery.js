cb4a3e40d0c43c8d318b5434d6f5dfd3d730213e
Floryss Rubechi <floryss.rubechi@gmail.com>
2021-06-29 18:04:06 +0200

    css for laptop and smartphone finished

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -6,21 +6,8 @@ $(document).ready(function(){
     	$("#flashMessage").animate({opacity: '0', margin: '0', height: '0', padding: '0'}, 500);
     });
 
-    /*$(".buttonRed").mouseenter(function(){
-        $(".buttonRed").animate({color: 'blue'}, 500);
-    });
-
-    $(".buttonRed").mouseleave(function(){
-        $(".buttonRed").animate({border: 'none'}, 500);
-    });*/
-
     $("#menu").click(function(){
     	$("#vertical").toggle(0);
     });
-
-    /*$("#menu").toggle(
-        function(){$("#vertical").show(0);},
-        function(){$("#vertical").hide(0);},
-    );*/
 
 });
